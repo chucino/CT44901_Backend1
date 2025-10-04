14addd6119937bc7a90a1a6c5a36468f993188cb
chucino <congchuc03800@gmail.com>
2025-10-04 09:56:45 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+// import class ApiError tự định nghĩa
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -14,5 +17,17 @@ app.get("/", (req, res) => {
 // Register contact routes
 const contactRoutes = require("./app/routes/contact.route");
 app.use("/api/contacts", contactRoutes);
+
+// Middleware xử lý lỗi 404
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// Middleware xử lý lloix tập trung
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
